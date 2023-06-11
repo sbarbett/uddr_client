e84fdab5fc153af3735655a7c175a3cd8b6a531c
Shane <shane@barbetta.me>
2023-06-10 23:29:50 -0400

    Added new methods, updated docstrings, cleaned up connection module

--- client.py
+++ client.py
@@ -13,10 +13,12 @@ class Client:
     def aggregates(self, query_type: str, **kwargs) -> Dict:
         """
         Query the aggregates endpoint.
-        
+
         This method sends a request to the aggregates endpoint. 
-        The endpoint provides aggregate data based on the specified query type.
-        
+        The endpoint provides aggregate data based on the specified query type. 
+        The data shows the frequency of occurrences of different entities 
+        (e.g., domains, country codes, registrars, etc.) in the queried dataset.
+
         :param query_type: The type of aggregate query. Accepted values are:
             DOMAIN
             FQDN
@@ -25,7 +27,17 @@ class Client:
             REGISTRAR
         :param kwargs: Additional parameters. Currently supports 'top_count' 
                        which sets how many aggregate values to get. The default is 25.
-        :return: The response from the server.
+        :return: An aggregate response {
+            :param query_type: The query type that the aggregate is for.
+            :param top_items: [{
+                :param current_doc_count: integer
+                    example: 9116
+                :param key: string
+                    example: debug.opendns.com
+                :param previous_doc_count: integer
+                    example: 7151
+            }]
+        }
         :raises ValueError: If query_type is not one of the accepted values or if top_count is not an integer.
         """
         uri = "/aggregates"
@@ -44,4 +56,83 @@ class Client:
                 raise ValueError("aggregates: top_count must be an integer")
             applied_filters.update({'top_count': i})
 
+        return self.connection.post(uri, json.dumps({'applied_filters': applied_filters}))
+
+    def bar(self, query_type: str) -> Dict:
+        """
+        Query the bar chart endpoint.
+
+        This method sends a request to the bar chart endpoint. 
+        The endpoint provides bar chart data based on the specified query type. 
+        The bar chart shows how the number of queries changes over time for a specific type of queries.
+
+        :param query_type: The type of bar chart query to perform. Accepted values are:
+            BLOCK_QUERIES
+            NO_ANSWER_QUERIES
+            TOR_PROX_VPN_QUERIES
+            SUSPICIOUS_NAMESERVER_QUERIES
+        :return: A bar chart response object with the following params.
+        {
+            :param query_type: The query type that the bar chart is for.
+            :param top_items: [{
+                :param current_doc_count: integer
+                    example: 369
+                :param key: string
+                    example: 2023-06-10 00:00:00
+                :param previous_doc_count: integer
+                    example: 303
+                :param previous_key: string
+                    example: 2023-06-03 00:00:00
+            }]
+        }
+        :raises ValueError: If query_type is not one of the accepted values.
+        """
+        uri = "/bar"
+        applied_filters = {}
+        
+        VALID_QUERY_TYPE = {'BLOCK_QUERIES', 'NO_ANSWER_QUERIES', 'TOR_PROX_VPN_QUERIES', 'SUSPICIOUS_NAMESERVER_QUERIES'}
+        if query_type.upper() not in VALID_QUERY_TYPE:
+            raise ValueError("aggregates: query_type must be one of %r" % VALID_QUERY_TYPE)
+            
+        applied_filters.update({'query_type': query_type.lower()})
+        
+        return self.connection.post(uri, json.dumps({'applied_filters': applied_filters}))
+        
+    def histogram(self, query_type: str) -> Dict:
+        """
+        Query the histogram endpoint.
+        
+        This method sends a request to the histogram endpoint. 
+        The endpoint generates histogram data for the specified query type. 
+        Histograms are useful for providing a visual representation of the distribution 
+        of numerical data. This can help to understand trends, patterns, and outliers 
+        in the queried dataset. The two query types, QUERIES and BLOCKED_QUERIES, 
+        represent total queries and queries that were blocked, respectively.
+        
+        :param query_type: The type of histogram query to perform. Accepted values are:
+            QUERIES
+            BLOCKED_QUERIES
+        :return: A histogram response object with the following params.
+        {
+            :param query_type: The query type that the histogram is for.
+            :param top_items: [{
+                :param current_doc_count: integer
+                    example: 237
+                :param key: string
+                    example: 2023-06-06 08:30:00
+                :param previous_doc_count: integer
+                    example: 0
+            }]
+        }
+        :raises ValueError: If query_type is not one of the accepted values.
+        """
+        uri = "/histogram"
+        applied_filters = {}
+        
+        VALID_QUERY_TYPE = {'QUERIES', 'BLOCKED_QUERIES'}
+        if query_type.upper() not in VALID_QUERY_TYPE:
+            raise ValueError("aggregates: query_type must be one of %r" % VALID_QUERY_TYPE)
+            
+        applied_filters.update({'query_type': query_type.lower()})
+        
         return self.connection.post(uri, json.dumps({'applied_filters': applied_filters}))
--- connection.py
+++ connection.py
@@ -1,47 +1,46 @@
-import requests, json
-from typing import Dict, Union, Optional
+import requests
 
 class AuthError(Exception):
     def __init__(self, message):
         self.message = message
-
+        
     def __str__(self):
         return repr(self.message)
-
+        
 class Connection:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key):
         self.endpoint = 'https://ddr.ultradns.com/api/protect/ext'
         self.api_key = api_key
+        
+    def _build_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'X-API-Key': self.api_key
+        }
+        
+    def post(self, uri, data=None, accept='application/json'):
+        return self._do_call(uri, 'POST', data=data, accept=accept)
+        
+    def _do_call(self, uri, method, data=None, accept='application/json'):
+        headers = self._build_headers()
+        headers['Accept'] = accept
+        response = requests.request(
+            method, 
+            self.endpoint+uri, 
+            data=data, 
+            headers=headers
+        )
 
-    def is_json(self, rstring: str) -> bool:
-        try:
-            json.loads(rstring)
-        except ValueError as e:
-            return False
-        return True
-
-    def build_headers(self, content_type: str) -> Dict[str, str]:
-        result = {'Accept': 'application/json', 'X-API-Key': self.api_key}
-        if content_type:
-            result['Content-Type'] = content_type
-        return result
-
-    def post(self, uri: str, json_data: Optional[str] = None) -> Union[Dict, str]:
-        if json_data is not None:
-            return self._do_call(uri, 'POST', body=json_data)
-        else:
-            return self._do_call(uri, 'POST')
-
-    def _do_call(self, uri: str, method: str, params: Optional[Dict] = None, body: Optional[str] = None,
-                 files: Optional[str] = None, content_type: str = 'application/json') -> Union[Dict, str]:
-        r1 = requests.request(method, self.endpoint+uri, params=params, data=body,
-                              headers=self.build_headers(content_type), files=files)
-        if r1.status_code == requests.codes.no_content:
+        # Check for No Content
+        if response.status_code == requests.codes.no_content:
             return {}
-        elif r1.status_code != requests.codes.ok:
-            # or raise an exception
-            return {"error": "Server returned status code: {}".format(r1.status_code)}
-        elif self.is_json(r1.text):
-            return r1.json()
-        else:
-            return r1.text
+
+        # If Accept header is application/pdf, return raw response content
+        if accept == 'application/pdf':
+            return response.content
+
+        # Attempt to return JSON, if not possible return text.
+        try:
+            return response.json()
+        except ValueError:
+            return response.text
