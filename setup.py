d918b6ec36011019e42321a196bfd3837b844ee5
Shane <shane@barbetta.me>
2023-06-27 17:33:43 -0400

    Improvements to the IOC parser so I can work on the IOC checker repo, submitting a minor version bump to PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="uddr_client", 
-    version="0.1.0",  
+    version="0.2.1",  
     description="A Python client for the UDDR API.", 
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown', 
@@ -24,6 +24,6 @@ setup(
     install_requires=[
         "pandas>=2.0.2",
         "xmltodict>=0.13.0",
-	"python-decouple>=3.8",
+	    "python-decouple>=3.8",
     ],
 )
